dbf47019e2b2ed832e010a1954ea58c108316076
shinhwiii <sh0122451@gmail.com>
2024-01-23 19:40:53 +0900

    초기 완성

---
+++ Assets/Scripts/Jelly.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jelly
+{
+    public int ID;
+    public int Level;
+    public float EXP;
+
+    public Jelly(int id, int level, float exp)
+    {
+        this.ID = id;
+        this.Level = level;
+        this.EXP = exp;
+    }
+}
